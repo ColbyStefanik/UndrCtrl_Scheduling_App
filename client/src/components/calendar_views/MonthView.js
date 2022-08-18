a236c2a8142a981b31b047fdf52a4367bbc22acc
ramen4lyfe <do.quan.m@gmail.com>
2022-08-18 01:48:22 -0700

    install calendar

---
+++ client/src/components/calendar_views/MonthView.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import CreateModal from '../modals/CreateModal';
+
+const MonthView = () => {
+    
+  return (
+    <div className="App">
+        
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        customButtons={{
+            new: {
+              text: 'Create Event',
+              click: () => {
+                <CreateModal />
+                console.log('new event')
+                },
+            },
+          }}
+        headerToolbar={{
+            center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+          }}
+        
+      />
+    </div>  )
+}
+
+export default MonthView;
