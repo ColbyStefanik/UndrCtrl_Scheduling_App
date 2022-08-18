3a77543d60ba7e7b0b2956b7ccdde63610878182
ramen4lyfe <do.quan.m@gmail.com>
2022-08-18 00:28:00 -0700

    edited to camel case, added enum

--- server/models/TimeBlockCard.model.js
+++ server/models/TimeBlockCard.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const TimeBlockCardSchema = new mongoose.Schema({
-    game_name: {
+    gameName: {
         type: String,
         enum: ['Game 1','Game 2','Game 3', 'Gmae 4','Game 5'],
         required: [true, 'game name is required'],
@@ -11,25 +11,25 @@ const TimeBlockCardSchema = new mongoose.Schema({
         required: [true, 'date is required'],
         //minlength: [5, 'date length should be more than 4 characters']
     },
-    start_time: {
+    starTime: {
         type: Number,
         required: [true, 'start time is required'],
         min: [0, 'invalid start time'],
         max: [23, 'invalid start time']
     },
-    end_time: {
+    endTime: {
         type: Number,
         required: [true, 'end time is required'],
         min: [1, 'invalid end time'],
         max: [24, 'invalid end time']
     },
-    host_employee: {
+    hostEmployee: {
         type: String,
         enum: ['Employee 1','Employee 2','Employee 3', 'Employee 4','Employee 5'],
         required: [true, 'Host employee is required'],
         // minlength: [3, 'host employee name should be more than 3 characters']
     },
-    gm_employee: {
+    gmEmployee: {
         type: String,
         enum: ['Employee 1','Employee 2','Employee 3', 'Employee 4','Employee 5'],
         required: [true, 'Game master is required'],
