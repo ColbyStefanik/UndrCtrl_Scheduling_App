9b0f83b52067828df3e7999184866e10b7930129
ramen4lyfe <do.quan.m@gmail.com>
2022-08-17 23:54:07 -0700

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import MonthView from './components/calendar_views/MonthView'
+import WeekView from './components/calendar_views/WeekView'
+import DayView from './components/calendar_views/DayView'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container-fluid-xl">
+      <div className="row">
+        <div className="col">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<MonthView />} />
+              <Route path="/" element={<WeekView />} />
+              <Route path="/" element={<DayView />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   );
 }
