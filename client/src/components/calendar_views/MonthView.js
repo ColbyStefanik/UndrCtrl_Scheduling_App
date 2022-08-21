29a57964600df0b4a718e399289a01d530938966
Quang Tran <qtran67@yahoo.com>
2022-08-21 15:36:20 -0700

    fix merge changes that removed datesSet handler

--- client/src/components/calendar_views/MonthView.js
+++ client/src/components/calendar_views/MonthView.js
@@ -13,7 +13,7 @@ const MonthView = () => {
   return (
     <div className="App">
         
-      {/* <FullCalendar
+      { <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         customButtons={{
@@ -28,8 +28,28 @@ const MonthView = () => {
         headerToolbar={{
             center: 'dayGridMonth,timeGridWeek,timeGridDay new',
           }}
+          datesSet={ function(info) { //update events list when month is changed
+            let y1 = info.view.currentStart.getFullYear();
+            let m1 = info.view.currentStart.getMonth();
+            let d1 = info.view.currentStart.getDate();
+            let y2 = info.view.currentEnd.getFullYear();
+            let m2 = info.view.currentEnd.getMonth();
+            let d2 = info.view.currentEnd.getDate();
+            let date1 = y1 + "-" + (m1+1) + "-" + d1;
+            let date2 = y2 + "-" + (m2+1) + "-" + (d2);
+
+            axios.get( `http://localhost:8000/api/calendar/range/${date1}/${date2}`)
+            .then((res=>{
+                //console.log(res);
+                console.log(res.data);
+                setEventList(res.data);
+            }))
+            .catch((err)=>console.log(err))
+
+            //console.log(date1 + " - " + date2);
+          }}
         
-      /> */}
+      /> }
     </div>  )
 }
 
