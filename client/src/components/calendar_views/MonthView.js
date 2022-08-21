b5809c0018bb9d17f883f152aa8264aea3a81807
Quang Tran <qtran67@yahoo.com>
2022-08-21 10:19:53 -0700

    Added event handler to update list of events for the current day/week/month.

--- client/src/components/calendar_views/MonthView.js
+++ client/src/components/calendar_views/MonthView.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import axios from 'axios';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,6 +7,8 @@ import interactionPlugin from '@fullcalendar/interaction';
 import CreateModal from '../modals/CreateModal';
 
 const MonthView = () => {
+
+  const [eventList, setEventList] = useState([])
     
   return (
     <div className="App">
@@ -24,6 +27,26 @@ const MonthView = () => {
           }}
         headerToolbar={{
             center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+          }}
+        datesSet={ function(info) { //update events list when month is changed
+            let y1 = info.view.currentStart.getFullYear();
+            let m1 = info.view.currentStart.getMonth();
+            let d1 = info.view.currentStart.getDate();
+            let y2 = info.view.currentEnd.getFullYear();
+            let m2 = info.view.currentEnd.getMonth();
+            let d2 = info.view.currentEnd.getDate();
+            let date1 = y1 + "-" + (m1+1) + "-" + d1;
+            let date2 = y2 + "-" + (m2+1) + "-" + (d2);
+
+            axios.get( `http://localhost:8000/api/calendar/range/${date1}/${date2}`)
+            .then((res=>{
+                //console.log(res);
+                console.log(res.data);
+                setEventList(res.data);
+            }))
+            .catch((err)=>console.log(err))
+
+            //console.log(date1 + " - " + date2);
           }}
         
       />
