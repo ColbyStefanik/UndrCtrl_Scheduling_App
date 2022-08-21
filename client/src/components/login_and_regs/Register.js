a31eb656a083a8f77ac92f11ad49cd9c9c4d2c6b
Quang Tran <qtran67@yahoo.com>
2022-08-21 16:45:52 -0700

    fix user routes. added styling to register

--- client/src/components/login_and_regs/Register.js
+++ client/src/components/login_and_regs/Register.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import {Link, useNavigate} from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = (props) => {
     const {usersList, setUsersList} = props;
@@ -16,45 +19,42 @@ const Register = (props) => {
     const onSubmitHandler = (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost:8000/api/user', {name, email, password})
+        axios.post('http://localhost:8000/api/user', {name, email, password, confirmPassword})
             .then(res=>{
                 console.log(res);
                 console.log(res.data);
                 navigate("/calendar/month");
             })
-            .catch((err) => setErrors(err.response.data.error.errors))     
+            .catch((err) => {console.log(err.response);setErrors(err.response.data.error.errors)})     
     }
     
     return (
         <div>
-            <div className="login_box">
-                <h3>Register</h3>
-                <div className="form-group">
-                    <form onSubmit={onSubmitHandler}>
-                        <div className='bold-font'>
-                            <label>Name:</label><br/>
-                            <input type="text" value={name} onChange = {(e)=>setName(e.target.value)}/>
-                        </div>
-                        {errors.name && <span style={{color:"red"}}>{errors.name.message}</span>}
-                        <div className='bold-font'>
-                            <label>Email:</label><br/>
-                            <input type="text" value={email} onChange = {(e)=>setEmail(e.target.value)}/>
-                        </div>
-                        {errors.email && <span style={{color:"red"}}>{errors.email.message}</span>}
-                        <div>
-                            <label>Password:</label><br/>
-                            <input type="password" value={password} onChange = {(e)=>setPassword(e.target.value)}/>
-                        </div>
-                        <div>
-                            <label>Confirm Password:</label><br/>
-                            <input type="password" value={confirmPassword} onChange = {(e)=>setConfirmPassword(e.target.value)}/>
-                        </div>
-                        {errors.password && <span style={{color:"red"}}>{errors.password.message}</span>}
-                        <br/>
-                        <button variant="contained" type="submit">Register</button>
-                    </form>
-                </div>
-            </div>
+            <Card className="text-center shadow m-5" >
+                <Card.Body>
+                    <Card.Title>REGISTER</Card.Title>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Control value={name} onChange = {(e)=>setName(e.target.value)} type="input" placeholder="Enter name" />
+                            {errors.name && <span style={{color:"red"}}>{errors.name.message}</span>}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Control value={email} onChange = {(e)=>setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                            {errors.email && <span style={{color:"red"}}>{errors.email.message}</span>}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control value={password} onChange = {(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+                            {errors.password && <span style={{color:"red"}}>{errors.password.message}</span>}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                            <Form.Control value={confirmPassword} onChange = {(e)=>setConfirmPassword(e.target.value)} type="password" placeholder="Confirm Password" />
+                            {errors.confirmPassword && <span style={{color:"red"}}>{errors.confirmPassword.message}</span>}
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={onSubmitHandler}>Register</Button>
+                    </Form>
+                    
+                </Card.Body>
+            </Card>
         </div>
     )
 }
--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -1,18 +1,18 @@
 const UserController = require('../controllers/User.controller');
 
-module.exports = (app) => {
+/*module.exports = (app) => {
     app.get('/api/calendar', UserController.getAllUsers);
     app.get('/api/calendar/:id', UserController.getUserById);
     app.post('/api/calendar', UserController.createUser);
     app.put('/api/calendar/:id', UserController.updateUser);
     app.delete('/api/calendar/:id', UserController.deleteUser);
     app.post('/api/calendar/login', UserController.login);
-};
-// module.exports = (app) => {
-//     app.get('/api/user', UserController.getAllUsers);
-//     app.get('/api/user/:id', UserController.getUserById);
-//     app.post('/api/user', UserController.createUser);
-//     app.put('/api/user/:id', UserController.updateUser);
-//     app.delete('/api/user/:id', UserController.deleteUser);
-//     app.post('/api/user/login', UserController.login);
-// };
+};*/
+module.exports = (app) => {
+     app.get('/api/user', UserController.getAllUsers);
+     app.get('/api/user/:id', UserController.getUserById);
+     app.post('/api/user', UserController.createUser);
+     app.put('/api/user/:id', UserController.updateUser);
+     app.delete('/api/user/:id', UserController.deleteUser);
+     app.post('/api/user/login', UserController.login);
+ };
