99ad83289f7c4ffe5ec731249f2c8c491a3526f3
Quang Tran <qtran67@yahoo.com>
2022-08-23 12:09:58 -0700

    Moved CreateModal outside of FullCalendar

--- client/src/components/calendar_views/MonthView.js
+++ client/src/components/calendar_views/MonthView.js
@@ -8,11 +8,11 @@ import CreateModal from '../modals/CreateModal';
 
 const MonthView = () => {
 
-  const [eventList, setEventList] = useState([])
+  const [eventList, setEventList] = useState([]);
     
   return (
     <div className="App">
-        
+      <CreateModal />
       { <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
@@ -28,26 +28,26 @@ const MonthView = () => {
         headerToolbar={{
             center: 'dayGridMonth,timeGridWeek,timeGridDay new',
           }}
-          datesSet={ function(info) { //update events list when month is changed
-            let y1 = info.view.currentStart.getFullYear();
-            let m1 = info.view.currentStart.getMonth();
-            let d1 = info.view.currentStart.getDate();
-            let y2 = info.view.currentEnd.getFullYear();
-            let m2 = info.view.currentEnd.getMonth();
-            let d2 = info.view.currentEnd.getDate();
-            let date1 = y1 + "-" + (m1+1) + "-" + d1;
-            let date2 = y2 + "-" + (m2+1) + "-" + (d2);
+        datesSet={ function(info) { //update events list when month is changed
+          let y1 = info.view.currentStart.getFullYear();
+          let m1 = info.view.currentStart.getMonth();
+          let d1 = info.view.currentStart.getDate();
+          let y2 = info.view.currentEnd.getFullYear();
+          let m2 = info.view.currentEnd.getMonth();
+          let d2 = info.view.currentEnd.getDate();
+          let date1 = y1 + "-" + (m1+1) + "-" + d1;
+          let date2 = y2 + "-" + (m2+1) + "-" + (d2);
 
-            axios.get( `http://localhost:8000/api/calendar/range/${date1}/${date2}`)
-            .then((res=>{
-                //console.log(res);
-                console.log(res.data);
-                setEventList(res.data);
-            }))
-            .catch((err)=>console.log(err))
+          axios.get( `http://localhost:8000/api/calendar/range/${date1}/${date2}`)
+          .then((res=>{
+              //console.log(res);
+              console.log(res.data);
+              setEventList(res.data);
+          }))
+          .catch((err)=>console.log(err))
 
-            //console.log(date1 + " - " + date2);
-          }}
+          //console.log(date1 + " - " + date2);
+        }}
         
       /> }
     </div>  )
